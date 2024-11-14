1c5b0798d62a8b3464fdad94c1465d801d8793b8
Utkarsh Harshe <utkarshj0119@gmail.com>
2024-11-14 23:10:37 +0530

    added like controller

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -26,17 +26,57 @@ const toggleVideoLike = asyncHandler(async(req,res)=>{
 const toggleCommentLike = asyncHandler(async(req,res)=>{
     const {commentId} = req.params
      //TODO: toggle like on comment
+    if (!commentId) {
+        throw new ApiError(400,"commentId required!")
+    }
 
-     
+    const like = await Like.create({
+        comment:commentId,
+        likeBy:req.user
+    })
+    if (!like) {
+        throw new ApiError(404,"like not found!")
+    }
+
+    res
+    .status(200)
+    .json(new ApiResponse(200,like,"you like the comment"))
 })
 
 const toggleTweetLike = asyncHandler(async(req,res)=>{
     const {tweetId} = req.params
      //TODO: toggle like on tweet
+     if (!tweetId) {
+        throw new ApiError(400,"tweetId required!")
+     }
+
+     const like = await Like.create({
+        tweet:tweetId,
+        likeBy:req.user
+    })
+
+    if (!like) {
+        throw new ApiError(404,"like not added")
+    }
+    res
+    .status(200)
+    .json(new ApiResponse(200,like,"Success"))
 })
 
 const getLikedVideos = asyncHandler(async(req,res)=>{
     //TODO: get all liked videos
+    const { page = 1, limit = 10 } = req.query;
+    const parsedLimit = parseInt(limit)
+    const pageSkip = (page - 1) * parsedLimit;
+    const allLikedVideo = await Like.find({video:req.user._id}).skip(pageSkip).limit(parsedLimit)
+
+    if (!allLikedVideo) {
+        throw new ApiError(504, "Couldn't find likes video")
+    }
+
+    res
+    .status(200)
+    .json(new ApiResponse(200,allLikedVideo,"Success"))
 })
 
 export{
