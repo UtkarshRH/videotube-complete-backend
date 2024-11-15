17e82dc9c95e2cac1492f3a4114a91cc01d8f95c
Utkarsh Harshe <utkarshj0119@gmail.com>
2024-11-15 10:44:41 +0530

    added heathcheck

--- src/controllers/healthCheck.controller.js
+++ src/controllers/healthCheck.controller.js
@@ -4,6 +4,11 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 
 const healthCheck = asyncHandler(async(req,res)=>{
      //TODO: build a healthcheck response that simply returns the OK status as json with a message
+    res
+    .status(200)
+    .json({
+       message:"ok"
+    })
 })
 
 export{
